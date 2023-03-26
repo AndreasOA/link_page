a1baf2cfe44ad854be17cf848bea527212f8b8bb
Andreas Oberdammer <andreas.oberdammer+coding@gmail.com>
2023-03-27 00:41:33 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,14 @@
+var dayDiffCase = Math.floor((new Date(Date()).getTime() - new Date("02/09/2023").getTime()) / (1000 * 3600 * 24), 0); /* calc gap between last case */
+var dayDiffOp = Math.floor((new Date(Date()).getTime() - new Date("09/21/2021").getTime()) / (1000 * 3600 * 24), 0); /* calc gap between last operation */
+var dayDiffS2 = Math.floor((new Date(Date()).getTime() - new Date("03/22/2023").getTime()) / (1000 * 3600 * 24), 0); /* calc gap to s2 */
+
+function randombg(){
+  var random= Math.floor(Math.random() * 6) + 0;
+  var bigSize = ["url('http://placehold.it/300&text=banner1')",
+                 "url('http://placehold.it/300&text=banner2')",
+                 "url('http://placehold.it/300&text=banner3')",
+                 "url('http://placehold.it/300&text=banner4')",
+                 "url('http://placehold.it/300&text=banner5')",
+                 "url('http://placehold.it/300&text=banner6')"];
+  document.getElementById("random").style.backgroundImage=bigSize[random];
+}
